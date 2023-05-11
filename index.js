8d35f292e61de293cf40599be6b0e8c2dbc72279
helenamaltseva <helena.maltseva@gmail.com>
2023-05-11 23:09:59 +0300

    Craeted new files

---
+++ index.js
@@ -0,0 +1,42 @@
+    const playList = [
+      {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN"
+      },
+      {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY"
+      },
+      {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD"
+      },
+      {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER"
+      },
+      {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER"
+      },
+      {
+        author: "AC/DC",
+        song: "BACK IN BLACK"
+      },
+      {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU"
+      },
+      {
+        author: "METALLICA",
+        song: "ENTER SANDMAN"
+      }
+    ];
+    
+    const playlistElement = document.getElementById("playlist");
+    
+    playList.forEach(function(song, index) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${index + 1}. ${song.author} - ${song.song}`;
+      playlistElement.appendChild(listItem);
+    });
